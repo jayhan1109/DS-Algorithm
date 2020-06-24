83f868cca3da63ad8f247a9afdda83b0fd5df18e
Jeongho Han <jungho1109@gmail.com>
2020-06-23 20:02:52 -0700

    Sorting_Bubble

---
+++ Sorting_Bubble.js
@@ -0,0 +1,14 @@
+function bubbleSort(arr) {
+  for (let i = 0; i < arr.length - 1; i++) {
+    for (let j = 0; j < arr.length - i - 1; j++) {
+      if (arr[j] > arr[j + 1]) {
+        let tmp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = tmp;
+      }
+    }
+  }
+  return arr;
+}
+
+bubbleSort([37, 45, 29, 8]);
